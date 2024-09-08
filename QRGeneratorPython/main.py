8541958009378bebdf735959f116fcd0b844340a
Erick Avila <yahir7083@gmail.com>
2024-09-08 14:25:35 -0500

    sample image center

--- QRGeneratorPython/main.py
+++ QRGeneratorPython/main.py
@@ -1,6 +1,40 @@
-import qrcode
+"""
+App to generate QR Codes
+Author: @FernandoFong
+Contributors: @ErickAvila
+"""
 
-if __name__ == '__main__':
-    name = input("Escriba el nombre para generar su QR Code:")
-    img = qrcode.make("https://web.fciencias.unam.mx")
-    img.save(f"{name}.png")
+# Dependencies
+import qrcode  # Support for QR Codes
+from PIL import Image  # Support for images
+
+img_path: str = "center.png"
+img: Image = Image.open(img_path)
+
+width: int = 100
+width_percent: float = (width / float(img.size[0]))
+height: int = int((float(img.size[1]) * float(width_percent)))
+img = img.resize((width, height))
+QR_code = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=2,
+)
+
+url: str = "https://web.fciencias.unam.mx"
+
+QR_code.add_data(url)
+
+QR_code.make(fit=True)
+
+QR_img = QR_code.make_image(fill_color="black", back_color="white").convert("RGBA")
+
+position: tuple = ((QR_img.size[0] - img.size[0]) // 2, (QR_img.size[1] - img.size[1]) // 2)
+QR_img.paste(img, position)
+QR_img.save("QR_code.png")
+
+# if __name__ == '__main__':
+#     name = input("Escriba el nombre para generar su QR Code:")
+#     img = qrcode.make("https://web.fciencias.unam.mx")
+#     img.save(f"{name}.png")
